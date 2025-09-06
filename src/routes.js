b30c7cd0f703fa17a4b85c9f7f8594fcf4069fbc
ArisAdrian <arisadrian2003@gmail.com>
2025-09-06 08:12:56 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,34 @@
+const { deleteNoteByIdHandler } = require('./handler.js');
+const { editNoteByIdHandler } = require('./handler.js');
+const { getNoteByIdHandler } = require('./handler.js');
+const { addNoteHandler, getAllNotesHandler } = require('./handler.js');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
